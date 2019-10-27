66fd24db95f411d23c5180e7291b50f0b2dc1f0e
Tim Schwab <timothy.john.schwab@gmail.com>
2019-10-26 20:05:15 -0400

    fix stuff

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ const {app, BrowserWindow} = require('electron')
 
 function main() {
 	// Create main window
-	let win = new BrowserWindow({width: 800, height: 600})
-	win.loadFile('client/index.html')
+	let win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	})
+	win.loadFile('src/client/index.html')
 }
 
 // Start app
