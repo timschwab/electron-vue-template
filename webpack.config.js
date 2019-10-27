d467f14ce4ff07a62709ae94d9d5d7097dbb383b
Tim Schwab <timothy.john.schwab@gmail.com>
2019-10-26 22:25:30 -0400

    use tabs everywhere

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,23 @@
-'use strict'
 const { VueLoaderPlugin } = require('vue-loader')
 const path = require('path')
 
 module.exports = {
-  target: 'electron-renderer',
-  entry: [
-    './src/client/app.js'
-  ],
-  output: {
-    path: path.resolve(__dirname, './src/client/dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        use: 'vue-loader'
-      }
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin()
-  ]
+	target: 'electron-renderer',
+	entry: [
+		'./src/client/app.js'
+	],
+	output: {
+		path: path.resolve(__dirname, './src/client/dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				use: 'vue-loader'
+			}
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin()
+	]
 }
